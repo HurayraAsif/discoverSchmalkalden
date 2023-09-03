3d46f2c34befd225783474ace7a5730fc51d3c8e
HurayraAsif <abuhurayra.asifkhan@gmail.com>
2023-09-03 18:36:43 +0200

    Update MainActivity.java
    c2

--- app/src/main/java/com/example/discoverschmalkalden/MainActivity.java
+++ app/src/main/java/com/example/discoverschmalkalden/MainActivity.java
@@ -12,6 +12,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         // Hello Asif
-        // Hello Serhii
+        // Hello Serhii123
     }
 }
